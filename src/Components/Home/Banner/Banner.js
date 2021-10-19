0d1cc09f1319936f5fdb0a3e8b515ba6424c5fd9
MdWahiduzzamanEmon <wahedemon09@gmail.com>
2021-10-19 23:05:05 +0600

    update

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import banner_img from '../../../images/banner-img.png'
 import './Banner.css'
 import shape from '../../../images/sheap.png'
+import { Link } from 'react-router-dom';
 const Banner = () => {
     return (
       <div
@@ -15,7 +16,9 @@ const Banner = () => {
           <div className="" data-aos="fade-right">
             <h5 className="medium-text">Your health. Our priority.</h5>
             <h1 className="big-text fw-bold py-3">100% pure Pharmacy.</h1>
-            <button className="btn btn-color rounded-pill">Contact Us</button>
+            <Link to="/conatctus">
+              <button className="btn btn-color rounded-pill">Contact Us</button>
+            </Link>
           </div>
           <div className="mt-md-0 mt-3">
             <img src={banner_img} alt="" className="img-fluid" />
